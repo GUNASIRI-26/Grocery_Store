c941fbc2e435f58d03587fa265a0904aab0f4971
GUNA SIRI <siriguna2006@gmail.com>
2025-06-30 12:43:00 +0530

    Update login.js

--- backend/controllers/login.js
+++ backend/controllers/login.js
@@ -1,4 +1,5 @@
 const { asyncHandler } = require("../service/asyncHandler.js")
+
 const { User } = require("../models/user.model.js")
 let login = asyncHandler(async (req, res) => {
     const { email, password } = req.body
